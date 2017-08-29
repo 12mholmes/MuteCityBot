28e8b4aa42eaded93ae993c3b38883d9a71ca0e4
12mholmes <12mholmes@users.noreply.github.com>
2017-08-28 22:47:57 -0700

    Initial commit of MuteCityBot.js

---
+++ MuteCityBot.js
@@ -0,0 +1,48 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+//const config = require('./config.json');
+var muteCityPath = 'Path/To/Mute/City.mp3';
+var dispatcher;
+var connection;
+
+function PlayMuteCity() {
+	if(connection !== null) {
+		dispatcher = connection.playFile(muteCityPath);
+		dispatcher.on('end', () => {
+			if(connection !== null) {
+				PlayMuteCity(connection);
+			}
+		})
+	}
+}
+
+
+bot.on("ready", () => {
+	console.log('Bot started');
+});
+
+bot.on('voiceStateUpdate', (oldMember, newMember) => {
+	let newUserChannel = newMember.voiceChannel
+	let oldUserChannel = oldMember.voiceChannel
+
+	if(newUserChannel && newUserChannel.name === 'Mute City' && bot.voiceConnections.size === 0) {
+		console.log('enter voice chann');
+		//User joins voice cahnnel
+		if(newMember.voiceChannel.name === 'Mute City' && bot.voiceConnections.size === 0) {
+			console.log('enter mute city');
+			newMember.voiceChannel.join().then(connect => {
+				connection = connect;
+				bot.guilds.first().member(bot.user).setMute(false);
+				PlayMuteCity();
+			}).catch(console.log);
+		}
+	} else if(bot.voiceConnections.size !== 0 && bot.voiceConnections.first().channel.members.size === 1) {
+		//User leaves voice channel
+		console.log('exit voice chann');
+		bot.voiceConnections.first().channel.leave();
+		connection = null;
+		dispatcher = null;
+	}
+});
+
+bot.login('your token')
